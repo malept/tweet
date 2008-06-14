bc0a0970340be43d697114f59c968753850f3c30
Mark Lee <git@lazymalevolence.com>
2008-06-14 13:41:55 -0700

    Add 'Refresh' to the context menu; formatting fixes

--- src/tweet-awn-applet.c
+++ src/tweet-awn-applet.c
@@ -32,11 +32,31 @@
 typedef struct
 {
   AwnAppletSimple *applet;
+  GtkWidget       *vbox;
   GtkWidget       *dialog;
 /*AwnConfigClient *config;*/
   GtkWidget       *menu;
   gboolean         dialog_shown;
 } TweetAwnApplet;
+
+static gboolean
+tweet_applet_menu_do_refresh (GtkMenuItem *item,
+                              TweetVBox   *vbox)
+{
+  if (vbox->refresh_id)
+  {
+    g_source_remove (vbox->refresh_id);
+    vbox->refresh_id = 0;
+  }
+
+  tweet_vbox_refresh (vbox);
+
+  vbox->refresh_id =
+    g_timeout_add_seconds (tweet_config_get_refresh_time (tweet_vbox_get_config (vbox)),
+                           (GSourceFunc)tweet_vbox_refresh_timeout,
+                           vbox);
+  return TRUE;
+}
 
 static gboolean
 tweet_applet_menu_show_about (GtkMenuItem *item)
@@ -62,7 +82,7 @@ tweet_applet_onclick (GtkWidget      *applet,
 
         gtk_window_set_default_size (GTK_WINDOW (tweet->dialog), TWEET_VBOX_WIDTH, 600);
 
-        vbox = tweet_vbox_new ();
+        tweet->vbox = vbox = tweet_vbox_new ();
         TWEET_VBOX (vbox)->mode = TWEET_MODE_RECENT;
         gtk_widget_set_size_request (vbox, TWEET_VBOX_WIDTH, 600);
         gtk_container_add (GTK_CONTAINER (tweet->dialog), vbox);
@@ -91,9 +111,19 @@ tweet_applet_onclick (GtkWidget      *applet,
         GtkWidget *item;
         tweet->menu = awn_applet_create_default_menu (AWN_APPLET (tweet->applet));
         gtk_menu_set_screen (GTK_MENU (tweet->menu), NULL);
+        /* reload */
+        item = gtk_image_menu_item_new_from_stock (GTK_STOCK_REFRESH, NULL);
+        gtk_widget_show (item);
+        g_signal_connect (G_OBJECT (item), "activate",
+                          G_CALLBACK (tweet_applet_menu_do_refresh),
+                          TWEET_VBOX (tweet->vbox));
+        gtk_menu_shell_append (GTK_MENU_SHELL (tweet->menu), item);
+        /* about */
         item = gtk_image_menu_item_new_from_stock (GTK_STOCK_ABOUT, NULL);
         gtk_widget_show (item);
-        g_signal_connect (G_OBJECT (item), "activate", G_CALLBACK (tweet_applet_menu_show_about), NULL);
+        g_signal_connect (G_OBJECT (item), "activate",
+                          G_CALLBACK (tweet_applet_menu_show_about),
+                          NULL);
         gtk_menu_shell_append (GTK_MENU_SHELL (tweet->menu), item);
       }
       gtk_menu_popup (GTK_MENU (tweet->menu),
@@ -124,9 +154,11 @@ awn_applet_factory_initp (const gchar *uid, gint orient, gint height)
                                    gtk_icon_theme_load_icon (theme,
                                                              "document-send",
                                                              height, 0, NULL));
-  g_signal_connect (G_OBJECT (tweet->applet), "button-press-event", G_CALLBACK (tweet_applet_onclick), tweet);
+  g_signal_connect (G_OBJECT (tweet->applet), "button-press-event",
+                    G_CALLBACK (tweet_applet_onclick), tweet);
 
   tweet->dialog = NULL;
+  tweet->vbox = NULL;
   tweet->menu = NULL;
   tweet->dialog_shown = FALSE;
 
